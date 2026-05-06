1c1ac70d3eaaab984b2d6551bfb214470560a088
Ndikum Sabastine Ndifor <sebastienndifor@gmail.com>
2026-05-06 02:11:02 +0100

    Addesd local storage to hold items

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import { TodoList } from "./todoList"
 import "./style.css"
 
 export default function App(){
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue === null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
     setTodos(currentTodos => {
